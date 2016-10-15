88cb22f227dc5eeeb74968c38e6c313c9a41a01c
YTingWu <asdf7322461@gmail.com>
2016-10-15 14:43:24 +0800

    11 替 Client 模型類別的部分欄位加上一些 DisplayName 自訂顯示名稱

--- MVC5Course/Models/Client.cs
+++ MVC5Course/Models/Client.cs
@@ -11,7 +11,9 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Client
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,11 +23,26 @@ namespace MVC5Course.Models
         }
     
         public int ClientId { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "{0} 最大不得超過 {1} 個字元")]
+        [DisplayName("名")]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "{0} 最大不得超過 {1} 個字元")]
+        [DisplayName("中間名")]
         public string MiddleName { get; set; }
+        [Required]
+        [StringLength(5, ErrorMessage = "{0} 最大不得超過 {1} 個字元")]
+        [DisplayName("姓")]
         public string LastName { get; set; }
+        [Required]
+        [DisplayName("性別")]
+        [RegularExpression("[MF]", ErrorMessage = "Gender 欄位只能輸入 M 或 F")]
         public string Gender { get; set; }
+        [DisplayName("生日")]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}")]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
+        [DisplayName("信用評等")]
         public Nullable<double> CreditRating { get; set; }
         public string XCode { get; set; }
         public Nullable<int> OccupationId { get; set; }
