e3e87bd6f6fb85c79100b2763da1df2ff3a834ac
YTingWu <asdf7322461@gmail.com>
2016-10-15 14:01:56 +0800

    09 建立 Clients 控制器與相關頁面，改善 /Clients/Index 頁面的顯示

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -27,6 +27,7 @@ namespace MVC5Course.Models
         [Required]
         public Nullable<decimal> Price { get; set; }
         [Required]
+        [DisplayFormat(DataFormatString ="{0:#,#}")]
         public Nullable<bool> Active { get; set; }
         [Required]
         public Nullable<decimal> Stock { get; set; }
